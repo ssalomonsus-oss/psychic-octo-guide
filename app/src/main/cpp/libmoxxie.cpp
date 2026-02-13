4f15d2b0d9eeba96fc634614a16e477bdc1efca3
ssalomonsus-oss <ssalomonsus@gmail.com>
2026-02-13 22:23:14 +0100

    Update libmoxxie.cpp

--- app/src/main/cpp/libmoxxie.cpp
+++ app/src/main/cpp/libmoxxie.cpp
@@ -1,71 +1,33 @@
 #include <jni.h>
-#include "BNM/Loading.hpp"
-#include "include/Menu/ModMenu.hpp"
-#include "BNM/UnityStructures.hpp"
-#include "BNM/Method.hpp"
-#include "BNM/Class.hpp"
-#include "BNMResolve.hpp"
-#include "BNM/Il2CppHeaders.hpp"
-#include <BNMIncludes.hpp>
-#include <NotiLib/NotiLib.hpp>
-#include <httplib.h>
-#include <json.hpp>
-#include <random>
-#include <string>
-#include <BNM/Class.hpp>
-#include <BNM/Method.hpp>
-#include <BNM/Field.hpp>
+#include <android/log.h>
+#include <cstring>
 
-//----- Variables -----\\-
-//Methods -----
+#define LOG_TAG "libmoxxie"
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
 
-void (*Awake)(void*);
-void new_Awake(void* instance) {
-    Awake(instance);
-    ModMenu::Init();
+struct ModInfo {
+    const char* id;
+    const char* name;
+    const char* version;
+    const char* author;
+    const char* description;
+};
 
-    BNM_LOG_INFO("Menu Initialization Called");
+extern "C" void setup(ModInfo& info) {
+    info.id = "libmoxxie";
+    info.name = "libmoxxie";
+    info.version = "1.0.0";
+    info.author = "moxxie";
+    info.description = "HoldableTemplate Scotland2 test build";
+    LOGI("setup() called");
 }
 
-bool isUpdated = true;
-void (*LateUpdate)(void*);
-void new_LateUpdate(void* instance) {
-    LateUpdate(instance);
-    ModMenu::Update();
-
-    if (isUpdated) {
-        BNM_LOG_INFO(BNM_OBFUSCATE("Menu Update Called"));
-        isUpdated = false;
-    }
+extern "C" void load() {
+    LOGI("libmoxxie load() called — mod initialized");
 }
 
-void OnLoaded() {
-    BNM_LOG_INFO(BNM_OBFUSCATE("Hooking Methods Now!!"));
-
-    InvokeHook(Class(BNM_OBFUSCATE("GorillaLocomotion"), BNM_OBFUSCATE("Player"), Image(
-            BNM_OBFUSCATE("Assembly-CSharp.dll"))).GetMethod(BNM_OBFUSCATE("LateUpdate")), new_LateUpdate, LateUpdate);
-
-    InvokeHook(Class(BNM_OBFUSCATE("GorillaLocomotion"), BNM_OBFUSCATE("Player"), Image(
-            BNM_OBFUSCATE("Assembly-CSharp.dll"))).GetMethod(BNM_OBFUSCATE("Awake")), new_Awake, Awake);
-
-    BNM_LOG_INFO(BNM_OBFUSCATE("Hooked Methods..."));
-}
-
-JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, [[maybe_unused]] void *reserved) {
-    JNIEnv *env;
-
-    if (vm->GetEnv(reinterpret_cast<void **>(&env), JNI_VERSION_1_6) != JNI_OK) {
-        BNM_LOG_ERR("Failed to get JNI environment!");
-        return JNI_ERR;
-    }
-
-    BNM::Loading::AddOnLoadedEvent(OnLoaded);
-
-    BNM_LOG_INFO("loaded! Calling TryLoadByJNI...");
-    if (!BNM::Loading::TryLoadByJNI(env)) {
-        BNM_LOG_ERR("TryLoadByJNI failed!");
-        return JNI_ERR;
-    }
-
+extern "C" jint JNI_OnLoad(JavaVM* vm, void* reserved) {
+    LOGI("JNI_OnLoad fired");
     return JNI_VERSION_1_6;
 }
